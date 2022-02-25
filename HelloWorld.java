c10cce1ee9cd0dd869c514b95e92213762057c3a
STUCHELLBJ21 <stuchellbj21@gcc.edu>
2022-02-25 09:32:49 -0500

    updated by Brayden(me), did what was on my slides

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("hi");
+    public static void main(String []args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("please tell us your name");
+        String name = scan.nextLine();
+        System.out.println("Hello" + name);
     }
 
 }
